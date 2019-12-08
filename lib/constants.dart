988a31937576059524abdb904abf42030ae40932
Aaron <github@otacon.press>
2019-12-08 12:31:54 -0500

    Add constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,12 +5,31 @@ const kBottomContainerColor = const Color(0xFFEB1555);
 const kActiveCardBackgroundColor = const Color(0xFF1D1E33);
 const kInactiveCardBackgroundColor = const Color(0xFF111328);
 
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25,
+  fontWeight: FontWeight.bold,
+);
 const kLabelTextStyle = TextStyle(
   fontSize: 18,
   color: Color(0xFF8D8E98),
 );
-
 const kLabelNumber = TextStyle(
   fontSize: 50,
   fontWeight: FontWeight.w900,
+);
+const kTitleTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.bold,
+);
+const kResultTextStyle = TextStyle(
+  color: const Color(0xFF24D876),
+  fontSize: 22,
+  fontWeight: FontWeight.bold,
+);
+const kBMITextStyle = TextStyle(
+  fontSize: 100,
+  fontWeight: FontWeight.bold,
+);
+const kBodyTextStyle = TextStyle(
+  fontSize: 22,
 );
